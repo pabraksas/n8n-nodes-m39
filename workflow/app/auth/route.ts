920e411eed604434f331b3ae278f8aa2614c3b6d
valerian-borisovich <valerian.borisovich@gmail.com>
2025-05-04 10:14:40 +0200

    update

---
+++ workflow/app/auth/route.ts
@@ -0,0 +1,16 @@
+import { serve } from '@upstash/workflow/nextjs'
+
+export const { POST } = serve<string>(async (context) => {
+  if (context.headers.get('authentication') !== 'Bearer secretPassword') {
+    console.error('Authentication failed.')
+    return
+  }
+
+  await context.run('step1', async () => {
+    return 'output 1'
+  })
+
+  await context.run('step2', async () => {
+    return 'output 2'
+  })
+})
